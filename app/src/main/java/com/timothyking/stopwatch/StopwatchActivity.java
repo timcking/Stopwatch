347ee0e6206683bfc76ac43f04bd23429f6db367
Tim King <timcking@gmail.com>
2016-08-08 19:04:13 -0700

    Stopped p. 130

--- app/src/main/java/com/timothyking/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/timothyking/stopwatch/StopwatchActivity.java
@@ -3,6 +3,9 @@ package com.timothyking.stopwatch;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.os.Handler;
 
 public class StopwatchActivity extends AppCompatActivity {
 
@@ -13,6 +16,29 @@ public class StopwatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
+    }
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+
+                if (running) {
+                    seconds++;
+                }
+
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 
     //Start the stopwatch running when the Start button is clicked.
